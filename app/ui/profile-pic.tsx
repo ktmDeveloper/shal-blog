bb6b03f362e191ebc4e12085d5b13c97f62fc190
Shalindra Shrestha <shalindra.shrestha@gmail.com>
2026-04-07 15:20:04 +0000

    update pic name

--- app/ui/profile-pic.tsx
+++ app/ui/profile-pic.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { CodeBracketIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/font";
 
 export default function ProfilePic() {
@@ -6,8 +6,8 @@ export default function ProfilePic() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <CodeBracketIcon className="h-12 w-12" />
+      <p className="text-[44px] ml-2">Shal</p>
     </div>
   );
 }
