df57a0e5c29da0e75394573c73cd86653fe5709c
Shalindra Shrestha <shalindra.shrestha@gmail.com>
2026-04-07 15:07:30 +0000

    skeleton nextjs upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="mx-auto w-full max-w-4xl rounded-xl bg-gray-50 p-6 md:p-12">
+        <h1>Main page</h1>
+      </div>
+    </main>
+  );
+}
